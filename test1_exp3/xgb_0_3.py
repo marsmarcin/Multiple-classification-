ee4550d13c7f0850fbc2906b0c66b60076135237
marsmarcin <1363041498@qq.com>
2018-08-27 07:52:03 -0500

    Add files via upload

---
+++ test1_exp3/xgb_0_3.py
@@ -0,0 +1,40 @@
+import numpy as np
+import xgboost as xgb
+data = np.loadtxt('K:/Pattern/test1_exp3/xgb_3.data', delimiter=',')
+sz = data.shape
+
+train = data[:int(sz[0] * 0.7), :]
+test = data[int(sz[0] * 0.7):, :]
+
+train_X = train[:, 0:30]
+train_Y = train[:, 30]
+
+test_X = test[:, 0:30]
+test_Y = test[:, 30]
+
+xg_train = xgb.DMatrix(train_X, label=train_Y)
+xg_test = xgb.DMatrix(test_X, label=test_Y)
+param = {}
+param['objective'] = 'multi:softmax'
+param['eta'] = 0.1
+param['max_depth'] = 6
+param['silent'] = 1
+param['nthread'] = 4
+param['num_class'] = 6
+
+watchlist = [(xg_train, 'train'), (xg_test, 'test')]
+num_round = 1000
+bst = xgb.train(param, xg_train, num_round, watchlist);
+
+pred = bst.predict(xg_test);
+
+param['objective'] = 'multi:softprob'
+bst = xgb.train(param, xg_train, num_round, watchlist);
+
+yprob = bst.predict(xg_test).reshape(test_Y.shape[0], 6)
+ylabel = np.argmax(yprob, axis=1)  
+
+test_error = sum(int(ylabel[i]) != test_Y[i] for i in range(len(test_Y))) / float(len(test_Y))
+accuracy = 1 - test_error
+print(test_error)
+print(accuracy)
